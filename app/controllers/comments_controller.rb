8feb56b82ddc0d2c329b062f66f08fb0955fa589
Evan Goldman <pitlere@yahoo.com>
2019-09-11 17:27:55 +0000

    form submission

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,11 @@
 class CommentsController < ApplicationController
-  before_action :authenicate_user!
+  before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
-    redirect_to place_path(@place)
+    redirect_to place_path(@place) 
   end
-
-def delete
-  @place = Place.find(params[:place_id])
-  @place.destroy
-end
 
   private
 
@@ -18,4 +13,3 @@ end
     params.require(:comment).permit(:message, :rating)
   end
 end
-
